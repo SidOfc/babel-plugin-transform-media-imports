f7018e4b25b416ee1defb01fea92f78fe1abeb35
Sidney Liebrand <sidneyliebrand@gmail.com>
2019-08-04 23:18:01 +0200

    Create small plugin to handle media file imports

---
+++ index.js
@@ -0,0 +1,197 @@
+const path = require('path');
+const imgSize = require('image-size');
+const fs = require('fs');
+const md5 = require('md5-file');
+const {execFileSync} = require('child_process');
+
+// get-video-dimensions package is async only which
+// makes it harder to use with babel. This is a "sync"
+// copy of the original as of 04-08-2019.
+function videoSize(path) {
+    const result = execFileSync('ffprobe', [
+        '-v',
+        'error',
+        '-of',
+        'flat=s=_',
+        '-select_streams',
+        'v:0',
+        '-show_entries',
+        'stream=height,width',
+        path
+    ]).toString();
+    const width = parseInt(/width=(\d+)/.exec(result)[1] || 0);
+    const height = parseInt(/height=(\d+)/.exec(result)[1] || 0);
+    const type = path
+        .split('.')
+        .pop()
+        .toLowerCase();
+
+    return {width, height, type};
+}
+
+function createMediaObject(importedPath, {file, opts}) {
+    const {root, filename} = file.opts;
+    const rootDir = opts.baseDir ? path.resolve(opts.baseDir) : process.cwd();
+    const importingFileDir = filename ? path.dirname(filename) : root;
+    const mediaPath = importedPath.startsWith('/')
+        ? importedPath
+        : path.resolve(path.join(importingFileDir, importedPath));
+    const isVideo = mediaPath.match(/(?:mp4|webm|ogv)$/);
+    const {width, height, type} = (isVideo ? videoSize : imgSize)(mediaPath);
+    let pathname = mediaPath.replace(rootDir, '');
+    let base64 = null;
+
+    if (opts.md5) {
+        const splatOpts = opts.md5.constructor === Object ? opts.md5 : {};
+        const {delimiter, length} = {delimiter: '-', length: 35, ...splatOpts};
+        const [fname, ...rest] = path.basename(pathname).split('.');
+        const hsh = md5.sync(mediaPath).slice(0, Math.max(4, length));
+
+        pathname = path.join(
+            path.dirname(pathname),
+            `${fname}${delimiter}${hsh}.${rest.join('.')}`
+        );
+    }
+
+    if (opts.base64) {
+        const splatOpts = opts.base64.constructor === Object ? opts.base64 : {};
+        const fileSize = fs.statSync(mediaPath).size;
+        const {maxSize} = {maxSize: 8192, ...splatOpts};
+
+        if (maxSize > fileSize) {
+            const b64str = Buffer.from(fs.readFileSync(mediaPath)).toString('base64');
+            base64 = `data:${isVideo ? 'video' : 'image'}/${type};base64,${b64str}`;
+        }
+    }
+
+    return {
+        pathname,
+        src: base64 || pathname,
+        width: width,
+        height: height,
+        aspectRatio: parseFloat((width / height).toFixed(3)),
+        heightToWidthRatio: parseFloat((height / width).toFixed(3)),
+        type: type
+    };
+}
+
+function toBabelMediaObject(m, t) {
+    return {
+        pathname: t.stringLiteral(m.pathname),
+        src: t.stringLiteral(m.src),
+        type: t.stringLiteral(m.type),
+        width: t.numericLiteral(m.width),
+        height: t.numericLiteral(m.height),
+        aspectRatio: t.numericLiteral(m.aspectRatio),
+        heightToWidthRatio: t.numericLiteral(m.heightToWidthRatio)
+    };
+}
+
+module.exports = ({types: t}) => ({
+    name: 'transform-media-imports',
+    visitor: {
+        ExportNamedDeclaration(p) {
+            const transforms = [];
+            if (!p.node.source) return;
+            const {
+                specifiers,
+                source: {value: rawExportPath}
+            } = p.node;
+
+            if (rawExportPath.match(/\.(?:svg|a?png|jpe?g|gif|webm|mp4|ogv)/i)) {
+                const exps = specifiers.filter(t.isExportSpecifier);
+                const defaultExport =
+                    specifiers.find(t.isExportDefaultSpecifier) ||
+                    exps.find(({local}) => local.name === 'default');
+                const namedExports = exps.filter(({local}) => local.name !== 'default');
+                const media = toBabelMediaObject(
+                    createMediaObject(rawExportPath, this),
+                    t
+                );
+
+                if (defaultExport) {
+                    transforms.push(
+                        t.exportDefaultDeclaration(
+                            t.objectExpression(
+                                Object.entries(media).map(([k, v]) =>
+                                    t.objectProperty(t.identifier(k), v)
+                                )
+                            )
+                        )
+                    );
+                }
+
+                if (namedExports.length) {
+                    transforms.push(
+                        ...namedExports.map(namedExport =>
+                            t.exportNamedDeclaration(
+                                t.variableDeclaration('const', [
+                                    t.variableDeclarator(
+                                        t.identifier(namedExport.exported.name),
+                                        media[namedExport.local.name]
+                                    )
+                                ]),
+                                []
+                            )
+                        )
+                    );
+                }
+            }
+
+            if (transforms.length) {
+                p.replaceWithMultiple(transforms);
+                p.stop();
+            }
+        },
+
+        ImportDeclaration(p) {
+            const transforms = [];
+            const {
+                specifiers,
+                source: {value: rawImportPath}
+            } = p.node;
+
+            if (rawImportPath.match(/\.(?:svg|a?png|jpe?g|gif|webm|mp4|ogv)/i)) {
+                const defaultImport = specifiers.find(t.isImportDefaultSpecifier);
+                const namedImports = specifiers.filter(t.isImportSpecifier);
+                const media = toBabelMediaObject(
+                    createMediaObject(rawImportPath, this),
+                    t
+                );
+
+                if (defaultImport) {
+                    transforms.push(
+                        t.variableDeclaration('const', [
+                            t.variableDeclarator(
+                                t.identifier(defaultImport.local.name),
+                                t.objectExpression(
+                                    Object.entries(media).map(([k, v]) =>
+                                        t.objectProperty(t.identifier(k), v)
+                                    )
+                                )
+                            )
+                        ])
+                    );
+                }
+
+                transforms.push(
+                    ...namedImports
+                        .filter(namedImport => media[namedImport.imported.name])
+                        .map(namedImport =>
+                            t.variableDeclaration('const', [
+                                t.variableDeclarator(
+                                    t.identifier(namedImport.local.name),
+                                    media[namedImport.imported.name]
+                                )
+                            ])
+                        )
+                );
+            }
+
+            if (transforms.length) {
+                p.replaceWithMultiple(transforms);
+                p.skip();
+            }
+        }
+    }
+});
---
+++ test/plugin.js
@@ -0,0 +1,198 @@
+import assert from 'assert';
+import * as babel from '@babel/core';
+import fs from 'fs';
+import path from 'path';
+
+function transform(code, options = {}) {
+    return babel.transform(code, {
+        presets: [['@babel/preset-env', {modules: false}]],
+        plugins: [['@babel/plugin-proposal-export-default-from'], ['./index', options]]
+    }).code;
+}
+
+describe('babel-plugin-transform-media-imports', () => {
+    describe('with @babel/plugin-proposal-export-default-from', () => {
+        it('can default export a media file using default export from', () => {
+            const code = transform('export mediaFile from "test/files/media-file.jpg"');
+
+            assert.equal(
+                code,
+                [
+                    'var _mediaFile = {',
+                    '  pathname: "/test/files/media-file.jpg",',
+                    '  src: "/test/files/media-file.jpg",',
+                    '  type: "jpg",',
+                    '  width: 280,',
+                    '  height: 280,',
+                    '  aspectRatio: 1,',
+                    '  heightToWidthRatio: 1',
+                    '};',
+                    'export { _mediaFile as mediaFile };'
+                ].join('\n')
+            );
+        });
+    });
+
+    it('can default export a media file using named exports', () => {
+        const code = transform('export {default} from "test/files/media-file.jpg"');
+
+        assert.equal(
+            [
+                'export default {',
+                '  pathname: "/test/files/media-file.jpg",',
+                '  src: "/test/files/media-file.jpg",',
+                '  type: "jpg",',
+                '  width: 280,',
+                '  height: 280,',
+                '  aspectRatio: 1,',
+                '  heightToWidthRatio: 1',
+                '};'
+            ].join('\n'),
+            code
+        );
+    });
+
+    it('can export a property from the media file', () => {
+        const code = transform(
+            'export {width, height as h} from "test/files/media-file.jpg"'
+        );
+
+        assert.equal(
+            ['export const width = 280;', 'export const h = 280;'].join('\n'),
+            code
+        );
+    });
+
+    it('converts default import to object with information', () => {
+        const code = transform('import a from "test/files/media-file.jpg"');
+
+        assert.equal(
+            [
+                'var a = {',
+                '  pathname: "/test/files/media-file.jpg",',
+                '  src: "/test/files/media-file.jpg",',
+                '  type: "jpg",',
+                '  width: 280,',
+                '  height: 280,',
+                '  aspectRatio: 1,',
+                '  heightToWidthRatio: 1',
+                '};'
+            ].join('\n'),
+            code
+        );
+    });
+
+    it('converts named imports to variable declarations', () => {
+        const code = transform(
+            'import {pathname, width as aw, aspectRatio} from "test/files/media-file.jpg"'
+        );
+        assert.equal(
+            [
+                'var pathname = "/test/files/media-file.jpg";',
+                'var aw = 280;',
+                'var aspectRatio = 1;'
+            ].join('\n'),
+            code
+        );
+    });
+
+    describe('options', () => {
+        describe('baseDir', () => {
+            it('removes provided baseDir from output pathname', () => {
+                const code = transform(
+                    'import {pathname} from "test/files/media-file.jpg"',
+                    {
+                        baseDir: './test'
+                    }
+                );
+
+                assert.equal('var pathname = "/files/media-file.jpg";', code);
+            });
+
+            it('removes root path from output pathname when empty / unspecified', () => {
+                const code = transform(
+                    'import {pathname} from "test/files/media-file.jpg"'
+                );
+
+                assert.equal('var pathname = "/test/files/media-file.jpg";', code);
+            });
+        });
+
+        describe('md5', () => {
+            it('appends full md5 hash when {md5: true}', () => {
+                const code = transform(
+                    'import {pathname} from "test/files/media-file.jpg"',
+                    {
+                        md5: true
+                    }
+                );
+                assert.equal(
+                    'var pathname = "/test/files/media-file-9554735b59274a729f35768ce68ed80a.jpg";',
+                    code
+                );
+            });
+
+            it('can specify md5 length with {md5: {length: <positive number>}}', () => {
+                const code = transform(
+                    'import {pathname} from "test/files/media-file.jpg"',
+                    {
+                        md5: {length: 10}
+                    }
+                );
+
+                assert.equal(
+                    'var pathname = "/test/files/media-file-9554735b59.jpg";',
+                    code
+                );
+            });
+
+            it('can specify md5 delimiter with {md5: {delimiter: <char>}}', () => {
+                const code = transform(
+                    'import {pathname} from "test/files/media-file.jpg"',
+                    {
+                        md5: {length: 10, delimiter: '.'}
+                    }
+                );
+
+                assert.equal(
+                    'var pathname = "/test/files/media-file.9554735b59.jpg";',
+                    code
+                );
+            });
+        });
+
+        describe('base64', () => {
+            it('converts src attribute to base64 when {base64: true}', () => {
+                const code = transform('import {src} from "test/files/media-file.jpg"', {
+                    base64: true
+                });
+
+                const b64str = Buffer.from(
+                    fs.readFileSync('test/files/media-file.jpg')
+                ).toString('base64');
+
+                assert.equal(`var src = "data:image/jpg;base64,${b64str}";`, code);
+            });
+
+            it('skips files > 8kb by default', () => {
+                const code = transform('import {src} from "test/files/media-file.webm"', {
+                    base64: true
+                });
+
+                assert.equal(`var src = "/test/files/media-file.webm";`, code);
+            });
+
+            it('can override the maximum with {base64: {maxSize: <positive number>}}', () => {
+                const code = transform('import {src} from "test/files/media-file.webm"', {
+                    base64: {maxSize: 10000}
+                });
+
+                const b64str = Buffer.from(
+                    fs.readFileSync('test/files/media-file.webm')
+                ).toString('base64');
+
+                assert.equal(`var src = "data:video/webm;base64,${b64str}";`, code);
+            });
+        });
+    });
+});
